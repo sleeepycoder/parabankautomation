64e956de9740d1b396c8010aec84a9d07051348d
Pankaj Chauhan <pankajschauhan0592@gmail.com>
2026-01-20 13:49:12 +0530

    create login smoke

---
+++ cypress/e2e/smoke/login.smoke.cy.js
@@ -0,0 +1,22 @@
+import LoginPage from '../../support/pages/LoginPage';
+import HomePage from '../../support/pages/HomePage';
+
+describe('Login Tests @smoke', () => {
+  let user;
+  beforeEach(() => {
+    cy.fixture('userlogin').then(data => {
+      user = data;
+    });
+    cy.visit('/');
+  });
+
+  it('should login successfully', () => {
+    LoginPage.login(user.username, user.password);
+    HomePage.welcomeMessage().should('have.text', 'Welcome');
+  });
+
+  it('should fail login with invalid credentials', () => {
+    LoginPage.login('invalidUser', 'wrongPassword');
+    cy.get('.error').should('contain', 'The username and password could not be verified.');
+  });
+});
